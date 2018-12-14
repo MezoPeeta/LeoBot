eeb29774b8ac8d1610830300930a75d18b774b3e
Unknown <mezo49@hotmail.com>
2018-12-14 23:27:35 +0200

    youtube

--- Leo.py
+++ Leo.py
@@ -1,27 +1,28 @@
 import discord
 import asyncio
 
-
 client = discord.Client()
 
 @client.event
 async def on_ready():
+    await client.change_presence(game=discord.Game(name='and helping'))
     print('LEO IS HEREEEEE')
 
 @client.event
 async def on_message(message):
-    if message.content.startswith('!messages'):
+    if message.content.startswith('$messages'):
         counter = 0
         tmp = await client.send_message(message.channel, 'Calculating messages...')
-        async for log in client.logs_from(message.channel, limit=100):
+        async for log in client.logs_from(message.channel, limit = 100):
             if log.author == message.author:
                 counter += 1
 
         await client.edit_message(tmp, 'You have {} messages.'.format(counter))
 
         
-    elif message.content.startswith('!Bday'):
+    elif message.content.startswith('$bday'):
         await client.send_message(message.channel, "HAPPYBIRTHDAAAY!, Wish you the best day ever. Don't Forget to ENJOOOOOOOOOOOOOY :blush: :gift: :gift_heart:")
         await client.send_file(message.channel, 'E:\WorkSpace\Python\Leo\LeoBot\Gif\Bday.gif')
-    
+
+
 client.run('NTIyODA5MjAzNTM0ODU2MjIy.DvQYUA.lZ5IPIyBQGZBVcqtzaXTXgnuOu0')

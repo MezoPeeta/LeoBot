f3dd828ca1099a9a1fa7132bf4f65a91365afbe4
Unknown <mezo49@hotmail.com>
2018-12-14 21:17:19 +0200

    Editing Bot
    Bday with the Gif

--- Leo.py
+++ Leo.py
@@ -5,14 +5,11 @@ client = discord.Client()
 
 @client.event
 async def on_ready():
-    print('Logged in as')
-    print(client.user.name)
-    print(client.user.id)
-    print('------')
+    print('LEO IS HEREEEEE')
 
 @client.event
 async def on_message(message):
-    if message.content.startswith('!test'):
+    if message.content.startswith('!messages'):
         counter = 0
         tmp = await client.send_message(message.channel, 'Calculating messages...')
         async for log in client.logs_from(message.channel, limit=100):
@@ -23,5 +20,10 @@ async def on_message(message):
     elif message.content.startswith('!sleep'):
         await asyncio.sleep(5)
         await client.send_message(message.channel, 'Done sleeping')
+
+        
+    elif message.content.startswith('!Bday'):
+        await client.send_message(message.channel, "HAPPYBIRTHDAAAY!, Wish you the best day ever. Don't Forget to ENJOOOOOOOOOOOOOY :blush: :man_dancing: :gift_heart:")
+        await client.send_file(message.channel, 'E:\WorkSpace\Python\Leo\LeoBot\Gif\Bday.gif')
 
 client.run('NTIyODA5MjAzNTM0ODU2MjIy.DvQYUA.lZ5IPIyBQGZBVcqtzaXTXgnuOu0')

92eb796388637e3aad84e8598bbf49b6e4c737d1
Unknown <mezo49@hotmail.com>
2018-12-14 21:47:41 +0200

    Still editing

--- Leo.py
+++ Leo.py
@@ -1,6 +1,5 @@
 import discord
 import asyncio
-
 client = discord.Client()
 
 @client.event
@@ -17,13 +16,12 @@ async def on_message(message):
                 counter += 1
 
         await client.edit_message(tmp, 'You have {} messages.'.format(counter))
-    elif message.content.startswith('!sleep'):
-        await asyncio.sleep(5)
-        await client.send_message(message.channel, 'Done sleeping')
 
         
     elif message.content.startswith('!Bday'):
-        await client.send_message(message.channel, "HAPPYBIRTHDAAAY!, Wish you the best day ever. Don't Forget to ENJOOOOOOOOOOOOOY :blush: :man_dancing: :gift_heart:")
+        await client.send_message(message.channel, "HAPPYBIRTHDAAAY!, Wish you the best day ever. Don't Forget to ENJOOOOOOOOOOOOOY :blush: :gift: :gift_heart:")
         await client.send_file(message.channel, 'E:\WorkSpace\Python\Leo\LeoBot\Gif\Bday.gif')
 
+
+    
 client.run('NTIyODA5MjAzNTM0ODU2MjIy.DvQYUA.lZ5IPIyBQGZBVcqtzaXTXgnuOu0')

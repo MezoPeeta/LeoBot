6273fb0613af1986b21f6885aaa8ffcf1d29cd85
Unknown <mezo49@hotmail.com>
2019-02-01 00:48:20 +0200

    Edited

--- Leo.py
+++ Leo.py
@@ -1,29 +1,59 @@
 import discord
-import asyncio
+from discord.ext import commands
 
-client = discord.Client()
+bot = commands.Bot(command_prefix='$')
 
-@client.event
-async def on_ready():
-    await client.change_presence(game=discord.Game(name='and helping'))
-    print('LEO IS HEREEEEE')
+bot.remove_command("help")
 
-@client.event
-async def on_message(message):
-    if message.content.startswith('$messages'):
-        counter = 0
-        tmp = await client.send_message(message.channel, 'Calculating messages...')
-        async for log in client.logs_from(message.channel, limit = 100):
-            if log.author == message.author:
-                counter += 1
 
-        await client.edit_message(tmp, 'You have {} messages.'.format(counter))
+@bot.event
+async def on_ready():   
+    game = discord.Game("and helping")
 
-        
-    elif message.content.startswith('$bday'):
-        await client.send_message(message.channel, "HAPPYBIRTHDAAAY!, Wish you the best day ever. Don't Forget to ENJOOOOOOOOOOOOOY :blush: :gift: :gift_heart:")
-        await client.send_file(message.channel, 'E:\WorkSpace\Python\Leo\LeoBot\Gif\Bday.gif')
-        await client.add_reaction("bday", "💝")
-
+    await bot.change_presence(status=discord.Status.idle, activity=game)
     
-client.run('NTIyODA5MjAzNTM0ODU2MjIy.DvQYUA.lZ5IPIyBQGZBVcqtzaXTXgnuOu0')
+    print('Logged in as')
+
+    print("Leo")
+    
+    print(bot.user.id)
+    
+    print('------------------------')
+
+
+@bot.command()
+async def bday(ctx):
+    await ctx.send(r"Happy Bday!,Wish you the best day ever. Don't Forget to ENJOY:gift: :gift_heart:",
+                   file=discord.File(r'E:\WorkSpace\Python\Leo\LeoBot\Bday\Images\Bday.gif',
+                                 r'E:\WorkSpace\Python\Leo\LeoBot\Bday\Images\hday.jpg'))
+    await ctx.message.add_reaction("💝")
+
+
+@bot.command()
+async def help(ctx):
+    author = ctx.message.author
+    embed = discord.Embed(
+        colour = discord.Colour.dark_magenta()
+    )
+
+    embed.set_author(name="Help")
+    embed.add_field(name="bday", value="Birthday",inline=False)
+
+    await author.send(author, embed=embed)
+
+@bot.command()
+async def clean(ctx):
+    textchannel = discord.TextChannel
+    channel = textchannel.purge
+    messages=[]
+    
+    async for message in channel.history(channel):
+        messages.append(message)
+
+    await discord.delete_messages(messages)
+    await ctx.say("Messages Deleted")
+
+
+
+bot.run('NTIyODA5MjAzNTM0ODU2MjIy.DvQYUA.lZ5IPIyBQGZBVcqtzaXTXgnuOu0')
+
